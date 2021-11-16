76aa01867f3da56306d8abef64c669c269f22f16
chadgazelem <bchadgazelem@gmail.com>
2021-11-16 11:27:29 +0800

    link

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const links = [
   { label: 'Week 6 Todo App', url: 'week6.html', },
   { label: 'Week 7 Notes', url: 'week7.html', },
   { label: 'Week 8 Notes', url: 'week8.html', },
-  { label: 'Week 9 Notes', url: 'week9.html', },
+  { label: 'Week 9 Notes' + '/' + 'Challenge Proposal', url: 'week9.html',url: 'proposal.html', },
   { label: 'Challenge Proposal', url: 'proposal.html', },
   { label: 'Week 10 Notes', url: 'week10.html', },
 ];
